5d766df4d94cef73c0fd5391d0ee536f1da210ce
Michelle Todd <michelle@khanacademy.org>
2015-12-09 07:51:37 -0800

    Move blog post list to a separate file

--- index.jsx
+++ index.jsx
@@ -10,6 +10,7 @@ const Route = Router.Route;
 const RouteHandler = Router.RouteHandler;
 
 import SS from "./styles/shared.js";
+import blogPostList from './posts/blog-post-list.js'
 
 import Blog from "./components/blog.js";
 import Post from "./components/post.js";
@@ -27,98 +28,7 @@ if (process.env.NODE_ENV === "development" && !appended) {
 const App = React.createClass({
   getInitialState: function() {
     return {
-      posts: [
-        {
-          date: "Sunday Dec 6, 2015",
-          title: "Math Facts part 5: Collecting data",
-          slug: "math-facts-part-5",
-          url: "../posts/math-facts-part-5.md",
-          tags: [
-            "math facts",
-          ],
-        },
-        {
-          date: "Tuesday Nov 24, 2015",
-          title: "A weird time-tracking experiment in which I plan out my entire week in advance",
-          slug: "time-tracking-experiment-nov-2015",
-          url: "../posts/time-tracking-experiment.md",
-          tags: [
-            "time tracking",
-            "experiments",
-          ],
-        },
-        {
-          date: "Friday Nov 20, 2015",
-          title: "The Martian: Advice for aspiring writers",
-          slug: "the-martian",
-          url: "../posts/the-martian.md",
-          tags: [
-            "no spoilers",
-            "drawings",
-            "advice",
-          ],
-        },
-        {
-          date: "Sunday Nov 8, 2015",
-          title: "How to get work done in an open office",
-          slug: "open-office",
-          url: "../posts/open-office.md",
-          tags: [
-            "advice",
-            "drawings",
-          ],
-        },
-        {
-          date: "Saturday Nov 7, 2015",
-          title: "Undertale",
-          slug: "undertale",
-          url: "../posts/undertale.md",
-          tags: [
-            "no spoilers",
-            "drawings",
-          ],
-        },
-        {
-          date: "September 2015",
-          title: "Math Facts part 4: The follow-up meeting",
-          written: "Thursday Dec 3, 2015",
-          slug: "math-facts-part-4",
-          url: "../posts/math-facts-part-4.md",
-          tags: [
-            "math facts",
-          ],
-        },
-        {
-          date: "June 2015",
-          title: "Math Facts part 3: The five-week solo project",
-          written: "Wednesday Nov 25, 2015",
-          slug: "math-facts-part-3",
-          url: "../posts/math-facts-part-3.md",
-          tags: [
-            "math facts",
-          ],
-        },
-        {
-          date: "April 2015",
-          title: "Math Facts part 2: The prototype-prototype",
-          written: "Monday Nov 16, 2015",
-          slug: "math-facts-part-2",
-          url: "../posts/math-facts-part-2.md",
-          tags: [
-            "math facts",
-          ],
-        },
-        {
-          date: "April 2015",
-          title: "Math Facts part 1: The idea",
-          written: "Wednesday Nov 11, 2015",
-          slug: "math-facts-part-1",
-          url: "../posts/math-facts-part-1.md",
-          tags: [
-            "math facts",
-          ],
-        },
-      ],
+      posts: blogPostList,
     };
   },
   componentDidMount: function() {
