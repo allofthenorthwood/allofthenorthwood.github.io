e567053663d8f3c429a889ac797a642b90f4bad6
Michelle Todd <michelle@khanacademy.org>
2015-12-27 10:41:58 -0800

    Update date

--- posts/blog-post-list.js
+++ posts/blog-post-list.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    date: "December 25, 2015",
+    date: "December 26, 2015",
     title: "Synesthesia: Reading in color",
     slug: "synesthesia",
     link: 'synesthesia?colors=%7B"0"%3A"%23313133"%2C"1"%3A"%23f9f6e5"%2C"2"%3A"%23d64286"%2C"3"%3A"%2345bf91"%2C"4"%3A"%23c72727"%2C"5"%3A"%238e5837"%2C"6"%3A"%23b93b70"%2C"7"%3A"%23f4bc47"%2C"8"%3A"%2336355f"%2C"9"%3A"%23653e74"%2C"a"%3A"%23d83a3a"%2C"l"%3A"%23fae08c"%2C"b"%3A"%233c4b71"%2C"c"%3A"%23fee15d"%2C"d"%3A"%238e6558"%2C"e"%3A"%236ba921"%2C"f"%3A"%23806d5f"%2C"g"%3A"%235b7712"%2C"h"%3A"%23de996a"%2C"i"%3A"%23fcf6d9"%2C"j"%3A"%238eaf88"%2C"k"%3A"%235f6370"%2C"m"%3A"%23b3335f"%2C"n"%3A"%23eb7f1c"%2C"o"%3A"%23343444"%2C"p"%3A"%235a367a"%2C"q"%3A"%2377655e"%2C"r"%3A"%23a0452d"%2C"s"%3A"%23d14680"%2C"t"%3A"%23738577"%2C"u"%3A"%23ddbd96"%2C"v"%3A"%23748875"%2C"w"%3A"%23687174"%2C"x"%3A"%23363c44"%2C"y"%3A"%23fbdb4a"%2C"z"%3A"%235a606e"%7D',
